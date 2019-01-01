077eacf2d5b07dcd8425a0836c01789f3045c5b7
aserfend <liuchangm7j@vip.qq.com>
2019-01-01 11:50:48 +0800

    浏览器交互初始化

---
+++ 订单信息服务器/WebSocketServer/WebSocketServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 订单信息服务器.WebSocketServer
+{
+	/// <summary>
+	/// 实现和浏览器的交互
+	/// </summary>
+	public class WebSocketServer
+	{
+	}
+}
