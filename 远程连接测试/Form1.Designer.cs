5f122dbd0a08a94d85500e07da88b0d8b852d448
aserfend <liuchangm7j@vip.qq.com>
2019-03-13 21:40:13 +0800

    New 服务器通信质量测试

--- 远程连接测试/Form1.Designer.cs
+++ 远程连接测试/Form1.Designer.cs
@@ -31,6 +31,9 @@
 			this.textBox1 = new System.Windows.Forms.TextBox();
 			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// textBox1
@@ -50,19 +53,51 @@
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(385, 28);
+			this.button1.Location = new System.Drawing.Point(378, 31);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(95, 32);
 			this.button1.TabIndex = 2;
-			this.button1.Text = "button1";
+			this.button1.Text = "AsClient";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(378, 69);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(95, 32);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "AsServer";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(378, 107);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(347, 173);
+			this.textBox3.TabIndex = 4;
+			this.textBox3.Text = "{\"Title\":\"rpClientConnect\",\"Name\":\"null\",\"Type\":\"vps\",}";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(378, 286);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(95, 32);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Send";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(839, 556);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button2);
 			this.Controls.Add(this.button1);
 			this.Controls.Add(this.textBox2);
 			this.Controls.Add(this.textBox1);
@@ -78,6 +113,9 @@
 		private System.Windows.Forms.TextBox textBox1;
 		private System.Windows.Forms.TextBox textBox2;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
